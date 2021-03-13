89a5e1ac4fb6ad5e21a51895992c2b90bfc8d5ad
Angelica <Angelica137@users.noreply.github.com>
2021-03-13 22:15:23 +0000

    output persons details

--- TestPerson.java
+++ TestPerson.java
@@ -2,7 +2,11 @@
 public class TestPerson {
 
 	public static void main(String[] args) {
-		//create Person object here
+		// create Person object here
+		Person p2 = new Person();
+		p2.displayDetails();
+		p2.checkPulse();
+		p2.checkForAbuse();
 
 	}
 
