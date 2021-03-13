2db2195bb4a9f5c3428f513e96b4b646ae0c81e2
Angelica <Angelica137@users.noreply.github.com>
2021-03-13 21:38:47 +0000

    complete display method

--- Person.java
+++ Person.java
@@ -2,39 +2,40 @@
 /**
  * To demonstrate boolean.
  * 
- * @author (your name) 
+ * @author (your name)
  * @version (a version number or a date)
  */
-public class Person
-{
-	double cmHeight ;
+public class Person {
+	double cmHeight;
 	double kgWeight;
 	boolean smoker = true;
 	int weeklyAlcholUnits = 50;
-	final int RWAL = 21;   //RWAL - Recommended Weekly Alcohol Limit
+	final int RWAL = 21; // RWAL - Recommended Weekly Alcohol Limit
 	int restPulse = 260;
 
-	
-	public void checkPulse()
-	{
-	   boolean healthyPulse;
-	   healthyPulse = ((restPulse > 30) && (restPulse < 180));
-	   System.out.print("Healthy pulse = ");
-	   System.out.println(healthyPulse);
+	public void checkPulse() {
+		boolean healthyPulse;
+		healthyPulse = ((restPulse > 30) && (restPulse < 180));
+		System.out.print("Healthy pulse = ");
+		System.out.println(healthyPulse);
 	}
-	
-	public void checkForAbuse()
-	{
-	      boolean abuser ;
-	      abuser = ((weeklyAlcholUnits > RWAL) || (smoker) );
-	      System.out.print("Checking for abuse -- ");
-	      System.out.println(abuser);
-	
+
+	public void checkForAbuse() {
+		boolean abuser;
+		abuser = ((weeklyAlcholUnits > RWAL) || (smoker));
+		System.out.print("Checking for abuse -- ");
+		System.out.println(abuser);
+
 	}
-	
-	public void displayDetails()
-	{
-	   
+
+	public void displayDetails() {
+		// display all the details of a person
+		System.out.println(cmHeight);
+		System.out.println(kgWeight);
+		System.out.println(smoker);
+		System.out.println(weeklyAlcholUnits);
+		System.out.println(RWAL);
+		System.out.println(restPulse);
 	}
-	
+
 }
